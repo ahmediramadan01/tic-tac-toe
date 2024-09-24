45aaa0c0a48cbb24d98d9da4c47ca2acfd5fc23f
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2024-09-24 20:34:10 +0300

    Implement play game logic

--- index.js
+++ index.js
@@ -47,4 +47,75 @@ const gameController = (function () {
 
 	const playerX = Player("playerX", "x");
 	const playerO = Player("playerO", "o");
+
+	const announcementElement = document.querySelector(".announcement");
+	announcementElement.style.color = "#000";
+
+	let currentPlayer = playerX,
+		gameOver = false,
+		winner;
+
+	const playGame = (function () {
+		const checkForWin = function () {
+			const winningCombinations = [
+				[0, 1, 2],
+				[3, 4, 5],
+				[6, 7, 8],
+				[0, 3, 6],
+				[1, 4, 7],
+				[2, 5, 8],
+				[0, 4, 8],
+				[2, 4, 6],
+			];
+
+			for (const combination of winningCombinations) {
+				if (
+					gameBoard.getBoard()[combination[0]] !== "" &&
+					gameBoard.getBoard()[combination[0]] === gameBoard.getBoard()[combination[1]] &&
+					gameBoard.getBoard()[combination[0]] === gameBoard.getBoard()[combination[2]]
+				) {
+					return true;
+				}
+			}
+			return false;
+		};
+
+		const checkForTie = function () {
+			for (const marker of gameBoard.getBoard()) {
+				if (marker === "") {
+					return false;
+				}
+			}
+
+			return !checkForWin();
+		};
+
+		const handlePlayerMove = function (event) {
+			if (!gameOver) {
+				gameBoard.modifyBoard(event.target.dataset.cell, currentPlayer.marker);
+
+				gameBoard.renderCell(event.target.dataset.cell);
+
+				if (checkForWin()) {
+					gameOver = true;
+					winner = currentPlayer;
+					announcementElement.textContent = winner === playerX ? "Player X WON!" : "Player O WON!";
+					return;
+				} else if (checkForTie()) {
+					gameOver = true;
+					announcementElement.textContent = "TIE!";
+					return;
+				}
+
+				currentPlayer = currentPlayer === playerX ? playerO : playerX;
+				announcementElement.textContent = currentPlayer === playerX ? "Player X's turn" : "Player O's turn";
+			}
+		};
+
+		gameBoard.getGameBoardElement().addEventListener("click", function (event) {
+			if (event.target.classList.contains("cell")) {
+				handlePlayerMove(event);
+			}
+		});
+	})();
 })();
