bbd0f6b75c6b778fa1aee3c94f6ce291f2193a06
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2025-01-05 09:26:57 +0200

    Build new game section

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 "use strict";
+
+(function () {
+  const gameNewSection = document.querySelector(".game--new");
+  const buttonNewGame = document.querySelector(".button--new");
+
+  const gameController = (function () {
+    const newGame = function () {
+      gameNewSection.style.display = "none";
+    };
+
+    return { newGame };
+  })();
+
+  buttonNewGame.addEventListener("click", gameController.newGame);
+})();
