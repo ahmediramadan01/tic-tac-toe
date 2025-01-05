452152e8e56c00a199bfcc0f6cf959f5ce5b5ae3
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2025-01-05 14:58:29 +0200

    Build start game section

--- index.js
+++ index.js
@@ -2,15 +2,51 @@
 
 (function () {
   const gameNewSection = document.querySelector(".game--new");
+  const gameStartSection = document.querySelector(".game--start");
   const buttonNewGame = document.querySelector(".button--new");
+  const choiceElement = document.querySelector(".choice");
+  const buttonStartGame = document.querySelector(".button--start");
+
+  const UIController = (function () {
+    const handleNewGameUI = function () {
+      gameNewSection.style.display = "none";
+      gameStartSection.style.display = "flex";
+    };
+
+    const getPlayer1 = () => document.querySelector('input[name="choice"]:checked').value;
+    const getPlayer2 = () => document.querySelector('input[name="choice"]:not(:checked)').value;
+
+    const toggleChoice = function () {
+      choiceElement.textContent = getPlayer1();
+    };
+
+    const handleStartGameUI = function () {
+      gameStartSection.style.display = "none";
+    };
+
+    return { handleNewGameUI, toggleChoice, getPlayer1, getPlayer2, handleStartGameUI };
+  })();
 
   const gameController = (function () {
     const newGame = function () {
-      gameNewSection.style.display = "none";
+      UIController.handleNewGameUI();
     };
 
-    return { newGame };
+    const createPlayer = function (name, marker) {
+      return { name, marker };
+    };
+
+    const startGame = function () {
+      UIController.handleStartGameUI();
+
+      const player1 = createPlayer("Player 1", UIController.getPlayer1());
+      const player2 = createPlayer("Player 2", UIController.getPlayer2());
+    };
+
+    return { newGame, startGame };
   })();
 
   buttonNewGame.addEventListener("click", gameController.newGame);
+  gameStartSection.addEventListener("change", UIController.toggleChoice);
+  buttonStartGame.addEventListener("click", gameController.startGame);
 })();
