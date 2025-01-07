b81dd97aa2e0e56fcd492376bd71f4d1cddeaa8c
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2025-01-07 19:43:13 +0200

    Implement playing round

--- index.js
+++ index.js
@@ -8,11 +8,15 @@
   const buttonStartGame = document.querySelector(".button--start");
   const gamePlaySection = document.querySelector(".game--play");
   const buttonResetGame = document.querySelector(".button--reset");
+  const gameBoardElement = document.querySelector(".game--play__board");
+  const cellElements = document.querySelectorAll(".board__cell");
+  const turnIcon = document.querySelector(".turn__icon");
 
   const UIController = (function () {
     const handleNewGameUI = function () {
       gameNewSection.style.display = "none";
       gameStartSection.style.display = "block";
+      gamePlaySection.style.display = "none";
     };
 
     const getPlayer1 = () => document.querySelector('input[name="choice"]:checked').value;
@@ -23,13 +27,43 @@
     };
 
     const handleStartGameUI = function () {
+      gameNewSection.style.display = "none";
       gameStartSection.style.display = "none";
       gamePlaySection.style.display = "block";
+
+      changeTurnIcon(getPlayer1());
     };
 
     const handleResetGameUI = function () {
+      gameNewSection.style.display = "flex";
+      gameStartSection.style.display = "none";
       gamePlaySection.style.display = "none";
-      gameStartSection.style.display = "flex";
+    };
+
+    const changeCellBackgroundColor = function (event) {
+      if (!event.target.classList.contains("board__cell")) return;
+
+      if (event.type === "mouseenter")
+        event.target.style.backgroundColor =
+          gameController.getCurrentPlayer().marker === "x"
+            ? "rgba(79, 195, 247, 0.16)"
+            : "rgba(255, 193, 7, 0.16)";
+      else if (event.type === "mouseleave") event.target.style.backgroundColor = "#fff";
+    };
+
+    const changeTurnIcon = function (currentMarker) {
+      turnIcon.src = `./images/${currentMarker}.svg`;
+    };
+
+    const renderCell = function (position) {
+      if (document.querySelector(`[data-cell="${position}"]`)) {
+        const currentMarker = gameController.gameBoard.getBoard()[position];
+        document.querySelector(
+          `[data-cell="${position}"]`
+        ).innerHTML = `<img src="./images/${currentMarker}.svg" alt="${currentMarker.toUpperCase()} icon" />`;
+
+        changeTurnIcon(currentMarker === "x" ? "o" : "x");
+      }
     };
 
     return {
@@ -39,10 +73,34 @@
       getPlayer2,
       handleStartGameUI,
       handleResetGameUI,
+      changeCellBackgroundColor,
+      changeTurnIcon,
+      renderCell,
     };
   })();
 
   const gameController = (function () {
+    let player1,
+      player2,
+      firstPlayer,
+      currentPlayer,
+      score1,
+      score2,
+      winner,
+      gameOver = true;
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    const gameBoard = (function () {
+      const board = ["", "", "", "", "", "", "", "", ""];
+      const getBoard = () => board;
+      const modifyBoard = function (position, marker) {
+        board[position] = marker;
+      };
+
+      return { getBoard, modifyBoard };
+    })();
+
     const newGame = function () {
       UIController.handleNewGameUI();
     };
@@ -52,21 +110,38 @@
     };
 
     const startGame = function () {
-      UIController.handleStartGameUI();
+      gameOver = false;
+      currentPlayer = firstPlayer = player1 = createPlayer("Player 1", UIController.getPlayer1());
+      player2 = createPlayer("Player 2", UIController.getPlayer2());
 
-      const player1 = createPlayer("Player 1", UIController.getPlayer1());
-      const player2 = createPlayer("Player 2", UIController.getPlayer2());
+      UIController.handleStartGameUI();
     };
 
     const resetGame = function () {
       UIController.handleResetGameUI();
+
+      currentPlayer = winner = undefined;
+      gameOver = true;
     };
 
-    return { newGame, startGame, resetGame };
+    const playRound = function (event) {
+      if (!gameOver) {
+        gameBoard.modifyBoard(event.target.dataset.cell, currentPlayer.marker);
+        UIController.renderCell(event.target.dataset.cell);
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+      }
+    };
+
+    return { getCurrentPlayer, newGame, startGame, resetGame, gameBoard, playRound };
   })();
 
   buttonNewGame.addEventListener("click", gameController.newGame);
   gameStartSection.addEventListener("change", UIController.toggleChoice);
   buttonStartGame.addEventListener("click", gameController.startGame);
   buttonResetGame.addEventListener("click", gameController.resetGame);
+  cellElements.forEach((cell) => {
+    cell.addEventListener("mouseenter", UIController.changeCellBackgroundColor);
+    cell.addEventListener("mouseleave", UIController.changeCellBackgroundColor);
+    cell.addEventListener("click", gameController.playRound);
+  });
 })();
